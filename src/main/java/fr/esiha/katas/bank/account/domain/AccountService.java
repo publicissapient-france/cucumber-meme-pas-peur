a65d1ddde4c49ae7db4ea211bd23b20a5f911a30
Édouard Siha <esiha@xebia.fr>
2020-06-20 14:32:34 +0200

    Implement Account deposit feature.

--- src/main/java/fr/esiha/katas/bank/account/domain/AccountService.java
+++ src/main/java/fr/esiha/katas/bank/account/domain/AccountService.java
@@ -2,14 +2,29 @@ package fr.esiha.katas.bank.account.domain;
 
 import fr.esiha.katas.bank.account.domain.account.Account;
 import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
 
+import java.time.Clock;
+
+import static java.lang.String.format;
+import static java.time.Clock.systemUTC;
 import static java.util.Objects.requireNonNull;
 
-public final class AccountService implements AccountOpeningService {
+public final class AccountService implements AccountOpeningService, AccountDepositService {
     private final AccountRepository repository;
+    private final Clock clock;
 
     public AccountService(final AccountRepository repository) {
+        this(repository, systemUTC());
+    }
+
+    AccountService(final AccountRepository repository, final Clock clock) {
         this.repository = requireNonNull(repository, "repository");
+        this.clock = requireNonNull(clock, "clock");
+    }
+
+    private static IllegalArgumentException unknownAccount(final Account.Id accountId) {
+        return new IllegalArgumentException(format("There is no account with identifier %s.", accountId));
     }
 
     @Override
@@ -17,5 +32,13 @@ public final class AccountService implements AccountOpeningService {
         requireNonNull(accountId, "accountId");
         requireNonNull(heldIn, "heldIn");
         repository.put(new Account(accountId, heldIn));
+    }
+
+    @Override
+    public void deposit(final Account.Id accountId, final Money depositAmount) {
+        requireNonNull(accountId, "accountId");
+        requireNonNull(depositAmount, "depositAmount");
+        final var account = repository.get(accountId).orElseThrow(() -> unknownAccount(accountId));
+        account.deposit(depositAmount, clock.instant());
     }
 }
--- src/test/java/fr/esiha/katas/bank/account/domain/AccountServiceTest.java
+++ src/test/java/fr/esiha/katas/bank/account/domain/AccountServiceTest.java
@@ -1,17 +1,27 @@
 package fr.esiha.katas.bank.account.domain;
-
 import fr.esiha.katas.bank.account.domain.account.Account;
 import fr.esiha.katas.bank.account.driven.inmemory.InMemoryAccountRepository;
+import org.joda.money.Money;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import java.time.Clock;
+import java.time.ZoneId;
+
+import static fr.esiha.katas.bank.account.domain.account.Operation.depositOf;
+import static java.lang.String.format;
+import static java.time.Clock.fixed;
+import static java.time.Clock.systemUTC;
+import static java.time.Instant.now;
+import static org.assertj.core.api.Assertions.*;
 import static org.joda.money.CurrencyUnit.EUR;
 import static org.joda.money.CurrencyUnit.USD;
+import static org.joda.money.Money.zero;
 
 public class AccountServiceTest {
     private final AccountRepository accountRepository = new InMemoryAccountRepository();
-    private final AccountService accountService = new AccountService(accountRepository);
+    private final Clock clock = fixed(now(), ZoneId.of("Europe/Paris"));
+    private final AccountService accountService = new AccountService(accountRepository, clock);
 
     @Test
     void should_be_final() {
@@ -19,37 +29,88 @@ public class AccountServiceTest {
     }
 
     @Test
-    void should_be_an_account_opening_service() {
-        assertThat(AccountOpeningService.class).isAssignableFrom(AccountService.class);
-    }
-
-    @Test
     void should_fail_to_create_from_null_account_repository() {
         assertThatNullPointerException()
-            .isThrownBy(() -> new AccountService(null))
+            .isThrownBy(() -> new AccountService(null, systemUTC()))
             .withMessage("repository");
     }
 
     @Test
-    void should_fail_to_open_account_with_null_account_id() {
+    void should_fail_to_create_from_null_clock() {
         assertThatNullPointerException()
-            .isThrownBy(() -> accountService.openAccount(null, EUR))
-            .withMessage("accountId");
+            .isThrownBy(() -> new AccountService(accountRepository, null))
+            .withMessage("clock");
     }
 
-    @Test
-    void should_fail_to_open_account_with_null_currency_unit() {
-        assertThatNullPointerException()
-            .isThrownBy(() -> accountService.openAccount(Account.Id.of("Germain"), null))
-            .withMessage("heldIn");
+    @Nested
+    class AccountOpeningServiceTest {
+        @Test
+        void should_be_an_account_opening_service() {
+            assertThat(AccountOpeningService.class).isAssignableFrom(AccountService.class);
+        }
+
+        @Test
+        void should_fail_to_open_account_with_null_account_id() {
+            assertThatNullPointerException()
+                .isThrownBy(() -> accountService.openAccount(null, EUR))
+                .withMessage("accountId");
+        }
+
+        @Test
+        void should_fail_to_open_account_with_null_currency_unit() {
+            assertThatNullPointerException()
+                .isThrownBy(() -> accountService.openAccount(Account.Id.of("Germain"), null))
+                .withMessage("heldIn");
+        }
+
+        @Test
+        void should_open_new_account() {
+            final var germain = Account.Id.of("Germain");
+            accountService.openAccount(germain, USD);
+
+            assertThat(accountRepository.get(germain))
+                .contains(new Account(germain, USD));
+        }
     }
 
-    @Test
-    void should_open_new_account() {
-        final var germain = Account.Id.of("Germain");
-        accountService.openAccount(germain, USD);
+    @Nested
+    class AccountDepositServiceTest {
+        @Test
+        void should_be_an_account_deposit_service() {
+            assertThat(AccountDepositService.class).isAssignableFrom(AccountService.class);
+        }
 
-        assertThat(accountRepository.get(germain))
-            .contains(new Account(germain, USD));
+        @Test
+        void should_fail_to_deposit_with_null_account_id() {
+            assertThatNullPointerException()
+                .isThrownBy(() -> accountService.deposit(null, zero(EUR)))
+                .withMessage("accountId");
+        }
+
+        @Test
+        void should_fail_to_deposit_with_null_money() {
+            assertThatNullPointerException()
+                .isThrownBy(() -> accountService.deposit(Account.Id.of("Harry"), null))
+                .withMessage("depositAmount");
+        }
+
+        @Test
+        void should_fail_to_deposit_on_unknown_account() {
+            final var accountId = Account.Id.of("Unknown");
+            assertThatIllegalArgumentException()
+                .isThrownBy(() -> accountService.deposit(accountId, Money.of(EUR, 300)))
+                .withMessage(format("There is no account with identifier %s.", accountId));
+        }
+
+        @Test
+        void should_deposit_amount_on_correct_account_at_current_time() {
+            final var accountId = Account.Id.of("Fred");
+            accountRepository.put(new Account(accountId, EUR));
+
+            accountService.deposit(accountId, Money.of(EUR, 250));
+
+            assertThat(accountRepository.get(accountId).orElseThrow().getOperations())
+                .endsWith(depositOf(Money.of(EUR, 250), clock.instant()));
+        }
     }
 }
