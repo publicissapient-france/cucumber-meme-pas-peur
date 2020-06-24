fded03a620a9e8c35b17389efa7e4ea2b890e979
Édouard Siha <esiha@xebia.fr>
2020-06-24 18:35:51 +0200

    Prepare dependency injection for DomainTest.

--- src/test/java/fr/esiha/katas/bank/account/DomainTest.java
+++ src/test/java/fr/esiha/katas/bank/account/DomainTest.java
@@ -1,7 +1,11 @@
 package fr.esiha.katas.bank.account;
 
+import fr.esiha.katas.bank.account.domain.AccountService;
+import fr.esiha.katas.bank.account.driven.inmemory.InMemoryAccountRepository;
+import io.cucumber.core.backend.ObjectFactory;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.picocontainer.PicoFactory;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -9,7 +13,37 @@ import org.junit.runner.RunWith;
     strict = true,
     features = "classpath:features",
     plugin = "pretty",
-    tags = "not @unimplemented"
+    tags = "not @unimplemented",
+    objectFactory = DomainTest.Configuration.class
 )
 public class DomainTest {
+    public static final class Configuration implements ObjectFactory {
+        private final ObjectFactory delegate;
+
+        public Configuration() {
+            delegate = new PicoFactory();
+            delegate.addClass(AccountService.class);
+            delegate.addClass(InMemoryAccountRepository.class);
+        }
+
+        @Override
+        public void start() {
+            delegate.start();
+        }
+
+        @Override
+        public void stop() {
+            delegate.stop();
+        }
+
+        @Override
+        public boolean addClass(final Class<?> glueClass) {
+            return delegate.addClass(glueClass);
+        }
+
+        @Override
+        public <T> T getInstance(final Class<T> glueClass) {
+            return delegate.getInstance(glueClass);
+        }
+    }
 }
